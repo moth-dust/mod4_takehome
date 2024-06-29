f8991b854c253037ba0fd6b28d692cc1aa61a907
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-06-29 14:20:36 -0600

    refactor: move mock data into src.

---
+++ takehome-challenge/src/mock_data/mock_data.js
@@ -0,0 +1,136 @@
+export const mockArticles = {
+    "status": "ok",
+    "totalResults": 10,
+    "articles": [
+      {
+        "source": {
+          "id": "bbc-news",
+          "name": "BBC News"
+        },
+        "author": "BBC News",
+        "title": "Trump evades debate question on whether he will accept election result",
+        "description": "Throughout the debate, Trump also repeated falsehoods on voter fraud, abortion and the 6 January riot.",
+        "url": "https://www.bbc.co.uk/news/articles/c51y85zwj1xo",
+        "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/1ef7/live/513d5ca0-3526-11ef-a044-9d4367d5b599.jpg",
+        "publishedAt": "2024-06-28T19:37:17.697994Z",
+        "content": "Trump made several assertions during the debate that either have no evidence or are factually wrong.\r\nOn abortion, Trump turned to what he called Democratic extremism - that the party wants to \"take … [+1428 chars]"
+      },
+      {
+        "source": {
+          "id": "bbc-news",
+          "name": "BBC News"
+        },
+        "author": "BBC News",
+        "title": "Singapore charges women who delivered pro-Palestinian letters",
+        "description": "The letters addressed to the prime minister appealed to the government to end ties with Israel.",
+        "url": "https://www.bbc.co.uk/news/articles/c72v5gn5404o",
+        "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/aba2/live/ab6f4440-3526-11ef-8e3c-639caf26c8a2.png",
+        "publishedAt": "2024-06-28T19:22:18.1678989Z",
+        "content": "Three activists in Singapore have been charged over rallying people to deliver letters to the prime minister urging him to end ties with Israel. \r\nSingapore strictly regulates protests, and public de… [+1918 chars]"
+      },
+      {
+        "source": {
+          "id": "bbc-news",
+          "name": "BBC News"
+        },
+        "author": "BBC News",
+        "title": "Supreme Court rules cities can ban homeless from sleeping outside",
+        "description": "The court found that the ban did not violate the constitution's limits on cruel and unusual punishment.",
+        "url": "https://www.bbc.co.uk/news/articles/cj774nxrpy7o",
+        "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/578f/live/ee489b40-357b-11ef-a044-9d4367d5b599.jpg",
+        "publishedAt": "2024-06-28T19:22:16.5901678Z",
+        "content": "The US Supreme Court has ruled that cities can ban homeless people from sleeping rough.\r\nIt is the court's most significant decision on homelessness since at least the 1980s, when many experts say th… [+2218 chars]"
+      },
+      {
+        "source": {
+          "id": "bbc-news",
+          "name": "BBC News"
+        },
+        "author": "BBC News",
+        "title": "What is the Chevron deference and why has it been overruled?",
+        "description": "The Supreme Court has overturned a longstanding legal precedent, and weakened agencies like the EPA.",
+        "url": "https://www.bbc.co.uk/news/articles/c51ywwrq45qo",
+        "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/0395/live/e65b05c0-3577-11ef-8eae-bb05c480678e.jpg",
+        "publishedAt": "2024-06-28T18:52:15.4959082Z",
+        "content": "While Chevron deference may sound like a chess strategy, it actually refers to a landmark Supreme Court ruling, Chevron v. Natural Resources Defense Council.\r\nThe court decided in 1984 that judges sh… [+1080 chars]"
+      },
+      {
+        "source": {
+          "id": "bbc-news",
+          "name": "BBC News"
+        },
+        "author": "BBC News",
+        "title": "Supreme Court rules against Jan 6 riot obstruction charges",
+        "description": "The court rules the charges must include proof that defendants tried to tamper with or destroy documents.",
+        "url": "https://www.bbc.co.uk/news/articles/c199l00gmmvo",
+        "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/0d72/live/07167350-28dd-11ef-baa7-25d483663b8e.jpg",
+        "publishedAt": "2024-06-28T16:37:15.4796971Z",
+        "content": "Federal prosecutors were wrong to use an obstruction law to charge hundreds of January 6 rioters, the supreme court has ruled in an opinion that could affect the case against Donald Trump.\r\nThe justi… [+1309 chars]"
+      },
+      {
+        "source": {
+          "id": "bbc-news",
+          "name": "BBC News"
+        },
+        "author": "BBC News",
+        "title": "Can Biden be replaced as Democrat nominee? Who could replace him?",
+        "description": "A poor debate performance has fuelled Democrat unease about Mr Biden's candidacy.",
+        "url": "https://www.bbc.co.uk/news/articles/c9wv821nljwo",
+        "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/b838/live/a37f0060-3506-11ef-9127-77ce7457cfd0.jpg",
+        "publishedAt": "2024-06-28T13:07:18.355Z",
+        "content": "The Democratic Party's nominee will officially be chosen in August at the Democratic National Convention (DNC).\r\nThere, a candidate must win support from the majority of \"delegates\" - party officials… [+859 chars]"
+      },
+      {
+        "source": {
+          "id": "bbc-news",
+          "name": "BBC News"
+        },
+        "author": "BBC News",
+        "title": "North Korea censors sunglasses, weddings and slang - report",
+        "description": "The South Korean report uses defectors' accounts to build a picture of human rights in the North.",
+        "url": "https://www.bbc.co.uk/news/articles/c9r3pv1dyg2o",
+        "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/8844/live/a9fa3d30-3536-11ef-a647-6fc50b20e53e.jpg",
+        "publishedAt": "2024-06-28T11:22:18.5427638Z",
+        "content": "What exactly the punishment is for these infractions is unclear.\r\nHowever, the crackdown on South Korean-made culture appears more severe.\r\nA 2020 law made watching or distributing South Korean enter… [+1075 chars]"
+      },
+      {
+        "source": {
+          "id": "bbc-news",
+          "name": "BBC News"
+        },
+        "author": "BBC News",
+        "title": "Indira Gandhi International Airport: Roof collapse smashes cars in Delhi",
+        "description": "Videos show crushed cars and broken pillars after a canopy over Terminal 1 collapses, killing one person.",
+        "url": "https://www.bbc.co.uk/news/articles/c725ny6p960o",
+        "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/9111/live/73f7b260-350b-11ef-b606-993eb74348a6.jpg",
+        "publishedAt": "2024-06-28T08:22:11.9642241Z",
+        "content": "A roof at Delhi airport has collapsed after heavy rain, killing one person and injuring four others.\r\nVideos online show huge pillars supporting the roof smashing into cars parked along the airport's… [+601 chars]"
+      },
+      {
+        "source": {
+          "id": "bbc-news",
+          "name": "BBC News"
+        },
+        "author": "BBC News",
+        "title": "Who won the Trump-Biden presidential debate?",
+        "description": "The president came into his debate with Donald Trump with a low bar to clear, and he struggled, writes Anthony Zurcher.",
+        "url": "https://www.bbc.co.uk/news/articles/crgedpw4r5eo",
+        "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/e09b/live/19b4c050-34f6-11ef-a328-d331e5fe750b.jpg",
+        "publishedAt": "2024-06-28T03:37:15.2773218Z",
+        "content": "Before Thursday evening, many Americans had expressed concerns about Joe Bidens age and fitness for office. To say that this debate did not put those concerns to rest may be one of the greatest under… [+1514 chars]"
+      },
+      {
+        "source": {
+          "id": "bbc-news",
+          "name": "BBC News"
+        },
+        "author": "BBC News",
+        "title": "Iran election: As young lose hope, a reformist runs for president",
+        "description": "As Iran votes for a new president, a reformist critical of the morality police has been topping polls.",
+        "url": "https://www.bbc.co.uk/news/articles/c98qgw8qxq7o",
+        "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/5222/live/64c77130-34b2-11ef-8557-bbe6d78d7b76.jpg",
+        "publishedAt": "2024-06-28T00:37:11.4181631Z",
+        "content": "Many Iranians have lost hope of any meaningful change being delivered through the ballot box.\r\nI wont vote this year, a 70-year old woman in Tehran, who has previously voted for reformist candidates,… [+1955 chars]"
+      }
+    ]
+  }
