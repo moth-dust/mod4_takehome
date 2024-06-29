10e3677e2db137b71b304780db9dd43f72714595
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-06-29 15:44:55 -0600

    refactor: do not include full article length in content.

--- takehome-challenge/src/components/ArticleSummaries/Details/Details.jsx
+++ takehome-challenge/src/components/ArticleSummaries/Details/Details.jsx
@@ -4,7 +4,7 @@ function Details({title, image, content, date, sourceName, sourceURL, changePoin
             <li className="headline">{title}</li>
             <li className="date">{date}</li>
             <li className="source-name">{sourceName}</li>
-            <li className="content">{content}</li>
+            <li className="content">"{content.split('[')[0]}"</li>
             <li className="source-url">Full Article:   <a href={sourceURL}>{sourceName}</a> </li>
           
             <button onClick={(e)=>{changePointer(0)}}>collapse details ...</button>
