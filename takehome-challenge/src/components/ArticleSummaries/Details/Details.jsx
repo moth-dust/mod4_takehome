49c6e0b23875aa069fd5923b50db69eda26e17b2
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-06-29 15:33:22 -0600

    feat: uses all details available.

--- takehome-challenge/src/components/ArticleSummaries/Details/Details.jsx
+++ takehome-challenge/src/components/ArticleSummaries/Details/Details.jsx
@@ -1,4 +1,16 @@
-function Details({}){
+function Details({title, image, content, date, sourceName, sourceURL, changePointer}){
+    return(
+        <ul className="details">
+            <li className="headline">{title}</li>
+            <li className="date">{date}</li>
+            <li className="source-name">{sourceName}</li>
+            <li className="content">{content}</li>
+            <li className="source-url">Full Article:   <a href={sourceURL}>{sourceName}</a> </li>
+          
+            <button onClick={(e)=>{changePointer(0)}}>collapse details ...</button>
+
+        </ul>
+    )
 
 }
 export default Details
