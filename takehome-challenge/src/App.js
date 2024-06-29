98173a02c07d2801f628db17bc1ea01a44dd047e
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-06-29 16:31:36 -0600

    feat: implements a simple live search based on article info.

--- takehome-challenge/src/App.js
+++ takehome-challenge/src/App.js
@@ -8,13 +8,20 @@ import ArticleSummaries from './components/ArticleSummaries/ArticleSummaries';
 function App() {
   const [articles, setArticles] = useState(mockArticles.articles)
   const [pairedArticles, setPairedArticles] = useState([])
+  const [query, setQuery] = useState('')
 
   useEffect(()=>{
-    setPairedArticles(formatToArray(articles))
-  },[articles])
+    let formatted = formatToArray(articles)
+    if(query){
+    formatted[0] = formatted[0].filter((article)=> article.searchString.includes(query.toLowerCase()))
+    }
+    setPairedArticles(formatted)
+  },[articles, query])
+
 
   return (
     <main>
+      <input placeholder='search for article' onChange={(e)=>{setQuery(e.target.value)}}></input>
       <ArticleSummaries pairedArticles = {pairedArticles}/>
     </main>
   );
