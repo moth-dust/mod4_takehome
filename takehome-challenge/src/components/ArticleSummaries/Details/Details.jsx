4830f839b6437534621e9abecc7eb6ea25afd2ec
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-06-29 18:02:01 -0600

    feat: add styling to page components, with minor tweaks to jsx class names for easier targetting.

--- takehome-challenge/src/components/ArticleSummaries/Details/Details.jsx
+++ takehome-challenge/src/components/ArticleSummaries/Details/Details.jsx
@@ -3,9 +3,9 @@ function Details({title, image, content, date, sourceName, sourceURL, changePoin
         <ul className="details">
             <img src={image}></img>
             <li className="headline">{title}</li>
-            <li className="date">{date}</li>
-            <li className="source-name">{sourceName}</li>
-            <li className="content">"{content.split('[')[0]}"</li>
+            <li className="date">Published: {date}</li>
+            <li className="source">{sourceName}</li>
+            <li className="description">"{content.split('[')[0]}"</li>
             <li className="source-url">Full Article:   <a href={sourceURL}>{sourceName}</a> </li>
             <button onClick={(e)=>{changePointer(0)}}>collapse details ...</button>
 
